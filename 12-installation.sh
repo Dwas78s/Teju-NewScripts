e0f5c021ebbbad5d1e89ee8210b10c18861bac34
Dwas78s <sumansivala02@gmail.com>
2024-04-18 19:24:12 +0530

    arrays

---
+++ 12-installation.sh
@@ -0,0 +1,25 @@
+#!bin/bash
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
+then 
+echo "You are not a root user, so I am going to exit here"
+exit 1
+else
+echo "You are a root user, so I am going to proceed with next steps"
+fi
+dnf installl mysql -y
+if [ $? -ne 0 ]
+then 
+echo "MYSQL package installation....Failure"
+exit 1
+else
+echo "MYSQL package installation....Success"
+fi
+dnf install git -y
+if [ $? -ne 0 ]
+then 
+echo "Git package installation....Failure"
+exit1
+else
+echo "Git package installation....Success"
+fi
