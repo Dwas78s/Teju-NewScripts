b59732af81267f2a1f02aaf953d64e0a142babc7
Dwas78s <sumansivala02@gmail.com>
2024-04-18 22:28:24 +0530

    functions

--- 17-colors.sh
+++ 17-colors.sh
@@ -12,9 +12,9 @@ VALIDATE()
 {
     if [ $1 -ne 0 ]
     then 
-echo -e "$2 ....$RFAILURE $N"
+echo -e "$2 ....$R FAILURE $N"
 else 
-echo -e "$2 .... $GSUCCESS $N"
+echo -e "$2 .... $G SUCCESS $N"
 fi
 }
 if [ $USERID -ne 0 ]
