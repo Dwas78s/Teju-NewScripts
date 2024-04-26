a7d49d2bf0a9e85baec2bcce2eaf3639db4e8164
Dwas78s <sumansivala02@gmail.com>
2024-04-26 16:08:01 +0530

    callingscripts

--- 28-Deletingolder-logs.sh
+++ 28-Deletingolder-logs.sh
@@ -3,5 +3,7 @@ SOURCEDIRECTORY=/tmp/app-logs
 if [ -d $SOURCEDIRECTORY ]
 then
 echo "Sourcedirectory doesnot exists:$SOURCEDIRECTORY"
-exit1
+exit 1
+else
+echo "Sourcedirectory exists:$SOURCEDIRECTORY"
 fi
