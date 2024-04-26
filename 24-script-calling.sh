93dbf298aa2d02216d2ab4464e4f45f29dda83b6
Dwas78s <sumansivala02@gmail.com>
2024-04-26 15:47:51 +0530

    callingscripts

--- 24-script-calling.sh
+++ 24-script-calling.sh
@@ -3,6 +3,7 @@ COURSE="Devops"
 echo "Variable from current script: $COURSE"
 echo "Process instance Id of current script:$$"
 
-./25-scriptother-calling.sh
+#./25-scriptother-calling.sh
 
+source ./25-scriptother-calling.sh
 echo "Variable from currentscript is : $COURSE"
