1d5b9374838c68af77a9338e42619b2083519529
Dwas78s <sumansivala02@gmail.com>
2024-04-28 20:04:33 +0530

    memory usage

--- 29-disk-usage.sh
+++ 29-disk-usage.sh
@@ -4,7 +4,7 @@ DISKTHRESHOLD=10
 
 while IFS=read -r line
 do
-USAGE=$(echo $line |awk -F " " '{print$6F}')|cut -d "%" -f1)
+USAGE=$(echo $line |awk -F " " '{print$6F}'|cut -d "%" -f1)
 FOLDER=$(echo $line|awk -F " " '{print$NF}')
 if [ $USAGE -ge $USAGE ]
 then
