54ab044267d5ad6802fc9a8454ef3f4bc5b9d0c0
Dwas78s <sumansivala02@gmail.com>
2024-04-28 20:28:34 +0530

    memory usage

---
+++ 30-emailnotification.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+DISKUSAGE=$(df -hT|grep xfs)
+DISKTHRESHOLD=10
+MESSAGE=""
+
+while IFS= read -r line
+do
+USAGE=$(echo $line |awk -F " " '{print$6F}'|cut -d "%" -f1)
+FOLDER=$(echo $line|awk -F " " '{print$NF}')
+if [ $USAGE -ge $USAGE ]
+then
+#echo "$FOLDER is morethan $DISKTHRESHOLD, current usage : $USAGE"
+MESSAGE+="$FOLDER is morethan $DISKTHRESHOLD, current usage : $USAGE \n"
+fi
+done <<<$DISKUSAGE
+echo -e "Message: $MESSAGE"
+echo "$MESSAGE"|mail -s "Diskusagereport" "ctejaswini05@gmail.com"
